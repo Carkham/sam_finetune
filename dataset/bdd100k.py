d42177aa344020ddb9a6812263de6df11eafc47e
Carkham <13193102258@163.com>
2024-04-23 01:09:53 +0800

    [feat] bdd100k

---
+++ dataset/bdd100k.py
@@ -0,0 +1,226 @@
+"""
+BDD100K Dataset Loader
+"""
+import logging
+import json
+import os
+import random
+
+import numpy as np
+from PIL import Image
+from skimage import color
+
+from torch.utils import data
+import torch
+import torchvision.transforms as transforms
+import cityscapes_labels as cityscapes_labels
+
+trainid_to_name = cityscapes_labels.trainId2name
+id_to_trainid = cityscapes_labels.label2trainid
+trainid_to_trainid = cityscapes_labels.trainId2trainId
+color_to_trainid = cityscapes_labels.color2trainId
+num_classes = 19
+ignore_label = 255
+groot = None
+img_postfix = '.jpg'
+
+palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153,
+           153, 153, 153, 250, 170, 30,
+           220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60,
+           255, 0, 0, 0, 0, 142, 0, 0, 70,
+           0, 60, 100, 0, 80, 100, 0, 0, 230, 119, 11, 32]
+zero_pad = 256 * 3 - len(palette)
+for i in range(zero_pad):
+    palette.append(0)
+
+
+def colorize_mask(mask):
+    """
+    Colorize a segmentation mask.
+    """
+    # mask: numpy array of the mask
+    new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
+    new_mask.putpalette(palette)
+    return new_mask
+
+
+def add_items(items, aug_items, img_path, mask_path, mask_postfix, mode):
+    """
+
+    Add More items ot the list from the augmented dataset
+    """
+
+    if mode == "train":
+        img_path = os.path.join(img_path, 'train')
+        mask_path = os.path.join(mask_path, 'train')
+    elif mode == "val":
+        img_path = os.path.join(img_path, 'val')
+        mask_path = os.path.join(mask_path, 'val')
+
+    list_items = [name.split(img_postfix)[0] for name in
+                  os.listdir(img_path)]
+    for it in list_items:
+        item = (os.path.join(img_path, it + img_postfix),
+                os.path.join(mask_path, it + mask_postfix))
+        items.append(item)
+
+
+def make_cv_splits(img_dir_name):
+    """
+    Create splits of train/val data.
+    A split is a lists of cities.
+    split0 is aligned with the default Cityscapes train/val.
+    """
+    trn_path = os.path.join(groot, img_dir_name, 'train')
+    val_path = os.path.join(groot, img_dir_name, 'val')
+
+    trn_cities = ['train/' + c for c in os.listdir(trn_path)]
+    val_cities = ['val/' + c for c in os.listdir(val_path)]
+
+    # want reproducible randomly shuffled
+    trn_cities = sorted(trn_cities)
+
+    all_cities = val_cities + trn_cities
+    num_val_cities = len(val_cities)
+    num_cities = len(all_cities)
+
+    cv_splits = []
+    for split_idx in range(cfg.DATASET.CV_SPLITS):
+        split = {}
+        split['train'] = []
+        split['val'] = []
+        offset = split_idx * num_cities // cfg.DATASET.CV_SPLITS
+        for j in range(num_cities):
+            if j >= offset and j < (offset + num_val_cities):
+                split['val'].append(all_cities[j])
+            else:
+                split['train'].append(all_cities[j])
+        cv_splits.append(split)
+
+    return cv_splits
+
+
+def make_split_coarse(img_path):
+    """
+    Create a train/val split for coarse
+    return: city split in train
+    """
+    all_cities = os.listdir(img_path)
+    all_cities = sorted(all_cities)  # needs to always be the same
+    val_cities = []  # Can manually set cities to not be included into train split
+
+    split = {}
+    split['val'] = val_cities
+    split['train'] = [c for c in all_cities if c not in val_cities]
+    return split
+
+
+def make_test_split(img_dir_name):
+    test_path = os.path.join(groot, img_dir_name, 'leftImg8bit', 'test')
+    test_cities = ['test/' + c for c in os.listdir(test_path)]
+
+    return test_cities
+
+
+def make_dataset(mode):
+    """
+    Assemble list of images + mask files
+
+    fine -   modes: train/val/test/trainval    cv:0,1,2
+    coarse - modes: train/val                  cv:na
+
+    path examples:
+    leftImg8bit_trainextra/leftImg8bit/train_extra/augsburg
+    gtCoarse/gtCoarse/train_extra/augsburg
+    """
+    items = []
+    aug_items = []
+
+    assert mode in ['train', 'val', 'test', 'trainval']
+    img_dir_name = 'images'
+    img_path = os.path.join(groot, img_dir_name)
+    mask_path = os.path.join(groot, 'labels')
+    mask_postfix = '.png'
+    # cv_splits = make_cv_splits(img_dir_name)
+    if mode == 'trainval':
+        modes = ['train', 'val']
+    else:
+        modes = [mode]
+    for mode in modes:
+        logging.info('{} fine cities: '.format(mode))
+        add_items(items, aug_items, img_path, mask_path, mask_postfix, mode)
+
+    # logging.info('Cityscapes-{}: {} images'.format(mode, len(items)))
+    logging.info('BDD100K-{}: {} images'.format(mode, len(items) + len(aug_items)))
+    return items, aug_items
+
+
+class BDD100K(data.Dataset):
+
+    def __init__(self, root='../data/cityscapes_video', **kwargs):
+        self.root = root
+        groot = root
+        self.mode = kwargs.get('split')
+        self.n_pos = 10
+        self.n_neg = 10
+
+        self.imgs, _ = make_dataset(self.mode)
+        if len(self.imgs) == 0:
+            raise RuntimeError('Found 0 images, please check the data set')
+
+        self.mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.trans_img = transforms.Compose([
+            transforms.Pad(padding=[0, 0, 0, 1024]),
+            transforms.ToTensor(),
+            transforms.Normalize(*self.mean_std)
+        ])
+
+    def get_point(self, gtf, n_pos, n_neg):
+        points = []
+        labels = []
+        for i in range(n_pos):
+            for j in range(20):
+                xh = random.randint(0, 511)
+                yw = random.randint(0, 1023)
+                if gtf[0, xh * 2 + 1, yw * 2 + 1] == 1:
+                    points.append([yw, xh])
+                    labels.append(1)
+                    break
+
+        for i in range(n_neg):
+            for j in range(20):
+                xh = random.randint(0, 511)
+                yw = random.randint(0, 1023)
+                if gtf[0, xh * 2 + 1, yw * 2 + 1] == 0:
+                    points.append([yw, xh])
+                    labels.append(-1)
+                    break
+        for i in range(20 - len(points)):
+            points.append([0, 0])
+            labels.append(-1)
+        return points, labels
+
+    def __getitem__(self, index):
+
+        img_path, mask_path = self.imgs[index]
+
+        img, mask = Image.open(img_path).convert('RGB'), Image.open(mask_path)
+        img_name = os.path.splitext(os.path.basename(img_path))[0]
+
+        mask = np.array(mask)
+        mask_copy = mask.copy()
+        for k, v in trainid_to_trainid.items():
+            mask_copy[mask == k] = v
+
+        gtf = torch.zeros_like(mask_copy)
+        gtf[mask_copy == 0] = 1
+        img_gtf = gtf
+
+        n_pos = random.randint(3, self.n_pos)
+        n_neg = 0 if self.n_neg == 0 else self.n_neg
+        points, labels = self.get_point(img_gtf, n_pos, n_neg)
+
+        return img, img_gtf, torch.tensor(points), torch.tensor(labels), img_path.split('/')[-1]
+
+    def __len__(self):
+        return len(self.imgs)
--- tools/eval.py
+++ tools/eval.py
@@ -12,8 +12,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torch.backends import cudnn
 import random
-from dataset import cityscapes
-from torch import distributed as dist
+from dataset import cityscapes, bdd100k
 from nets import sam
 import torch
 import os
@@ -26,7 +25,7 @@ device = None
 log_file = None
 
 
-def eval(args, root):
+def train(args, root):
     args_train = args['train']
     seed = args_train.get('seed', 2106346)
     # init_seeds(seed + local_rank)
@@ -37,49 +36,21 @@ def eval(args, root):
     log_file = open(os.path.join(root, "logs/log.txt"), "w")
     set_file(log_file, rank=local_rank)
     to_log(args)
-    dataset = args["eval"]["dataset"]
-    if dataset == "bdd100k":
-        
-    args_train = args["train"]
+    args_data = args["data"]
 
-    train_loader = DataLoader(cityscapes.csdata(
-        args_data["root"], split="train"), batch_size=args_data["bs"], num_workers=args_data["num_workers"])
+    valid_loader = DataLoader(bdd100k.BDD100K(args_data["root"], split="valid"), batch_size=50)
 
-    valid_loader = DataLoader(cityscapes.csdata(
-        args_data["root"], split="valid"), batch_size=args_data["bs"], num_workers=args_data["num_workers"])
-
-    sam_model = sam.get_model(args['train']).cuda()
-    opt = optim.AdamW(
-        sam_model.parameters(), lr=args_train['lr'])
-    if args['train']['adaptor'] == 'bias':
-        to_log("Bias tuning")
-        for k, p in sam_model.named_parameters():
-            if "image_encoder" in k and "bias" not in k:
-                p.requires_grad = False
-    elif args['train']['adaptor'] == 'adaptor':
-        to_log("Adaptor tuning")
-        for k, p in sam_model.named_parameters():
-            if ("image_encoder" in k and "prompt_generator" not in k) or ("prompt_encoder" in k):
-                p.requires_grad = False
-    else:
-        to_log("Mask Decoder tuning")
-        for k, p in sam_model.named_parameters():
-            if "image_encoder" in k:
-                p.requires_grad = False
-    sch = optim.lr_scheduler.PolynomialLR(
-        opt, args_train['total_iters'], power=args_train["power"])
+    sam_model = sam.get_model().cuda()
+    for k, p in sam_model.named_parameters():
+        p.requires_grad = False
 
     writer = SummaryWriter(os.path.join(root, "logs/result/event/"))
-
-    tot_iter = 0
-    while True:
-        if tot_iter == args_train["total_iters"]:
-            break
-        for img_org, img_gtf, points, labels, image_path in train_loader:
-            if tot_iter == args_train["total_iters"]:
-                break
-            sam_model.train()
-            tot_iter += 1
+    with torch.no_grad():
+        IoUs = 0.
+        Rs = 0.
+        losses = 0.
+        for img_org, img_gtf, points, labels, image_path in tqdm(valid_loader):
+            sam_model.eval()
             img_input = nn.functional.interpolate(
                 img_org, [1024, 1024], mode="bilinear").cuda()
             img_gtf, points, labels = img_gtf.cuda(), points.cuda(), labels.cuda()
@@ -103,103 +74,25 @@ def eval(args, root):
 
             from torch.nn.functional import threshold, normalize
 
-            binary_mask = normalize(threshold(upscaled_masks, 0.0, 0)).cuda()
+            binary_mask = normalize(
+                threshold(upscaled_masks, 0.0, 0)).cuda()
             upscaled_masks = nn.Sigmoid()(upscaled_masks)
-            loss = nn.BCELoss()(upscaled_masks, img_gtf.float())
-            opt.zero_grad()
-            loss.backward()
-            opt.step()
-            sch.step()
 
-            # from torchvision.transforms import transforms
-            # from PIL import ImageDraw
-            # a = transforms.ToPILImage()(binary_mask.squeeze(0))
-            # a.save("p.png")
-            # b = transforms.ToPILImage()(img_gtf.squeeze(0).float())
-            # b.save("gt.png")
-            # bb = ImageDraw.Draw(b)
-            # bb.point((int(points[0][0][0]), int(points[0][0][1])), fill="red")
-            # b.save("gt_.png")
-            # c = transforms.ToPILImage()(img_org.squeeze(0).float())
-            # c.save("org.png")
-            # print(points)
-            # exit(0)
+            losses += nn.BCELoss()(upscaled_masks, img_gtf.float())
 
-        #     for name, opt in data_pool['opt'].items():
-        #         opt.zero_grad()
-        #     tot_loss = 0.
-        #     for name, loss in data_pool['loss'].items():
-        #         if local_rank == 0:
-        #             writer.add_scalar("train/loss_{}".format(name), loss,
-        #                               data_pool['tot_iter'])
-        #         tot_loss += loss
-        #     tot_loss.backward()
-        #     for name, opt in data_pool['opt'].items():
-        #         opt.step()
-        #     for name, sch in data_pool['sch'].items():
-        #         sch.step()
-
-            if tot_iter % args_train['show_interval'] == 0 and local_rank == 0:
-                to_log("iter: {}, epoch: {}, batch: {}/{}, loss: {}".format(
-                    tot_iter, tot_iter // len(
-                        train_loader), tot_iter % len(train_loader),
-                    len(train_loader), loss))
-                writer.add_scalar("train/loss", loss, tot_iter)
-
-                writer.add_scalar("train/lr", opt.param_groups[0]['lr'], tot_iter)
-            if tot_iter % args_train['snapshot_interval'] == 0:
-                torch.save(sam_model.state_dict(), os.path.join(
-                    root, "logs", "model-iter-{}.pth".format(tot_iter)))
-
-            if tot_iter % args_train['valid_interval'] == 0 and local_rank == 0:
-                with torch.no_grad():
-                    IoUs = 0.
-                    Rs = 0.
-                    losses = 0.
-                    for img_org, img_gtf, points, labels, image_path in tqdm(valid_loader):
-                        sam_model.eval()
-                        img_input = nn.functional.interpolate(
-                            img_org, [1024, 1024], mode="bilinear").cuda()
-                        img_gtf, points, labels = img_gtf.cuda(), points.cuda(), labels.cuda()
-
-                        with torch.no_grad():
-                            image_embedding = sam_model.image_encoder(img_input)
-                            sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
-                                points=(points, labels),
-                                boxes=None,
-                                masks=None,
-                            )
-                        low_res_masks, iou_predictions = sam_model.mask_decoder(
-                            image_embeddings=image_embedding,
-                            image_pe=sam_model.prompt_encoder.get_dense_pe(),
-                            sparse_prompt_embeddings=sparse_embeddings,
-                            dense_prompt_embeddings=dense_embeddings,
-                            multimask_output=False,
-                        )
-                        upscaled_masks = sam_model.postprocess_masks(
-                            low_res_masks, [512, 1024], [1024, 2048]).cuda()
-
-                        from torch.nn.functional import threshold, normalize
-
-                        binary_mask = normalize(
-                            threshold(upscaled_masks, 0.0, 0)).cuda()
-                        upscaled_masks = nn.Sigmoid()(upscaled_masks)
-
-                        losses += nn.BCELoss()(upscaled_masks, img_gtf.float())
-
-                        I = (binary_mask == 1) & (img_gtf == 1)
-                        U = (binary_mask == 1) | (img_gtf == 1)
-                        IoU = I.sum() / (U.sum() + 1e-8)
-                        R = I.sum() / ((img_gtf == 1).sum() + 1e-8)
-                        IoUs += IoU
-                        Rs += R
-                IoU = IoUs / len(valid_loader)
-                R = Rs / len(valid_loader)
-                loss = losses / len(valid_loader)
-                to_log("valid: \nIoU: {}\nRecall: {}".format(IoU, R))
-                writer.add_scalar("valid/loss", loss, tot_iter)
-                writer.add_scalar("valid/IoU", IoU, tot_iter)
-                writer.add_scalar("valid/R", R, tot_iter)
+            I = (binary_mask == 1) & (img_gtf == 1)
+            U = (binary_mask == 1) | (img_gtf == 1)
+            IoU = I.sum() / (U.sum() + 1e-8)
+            R = I.sum() / ((img_gtf == 1).sum() + 1e-8)
+            IoUs += IoU
+            Rs += R
+    IoU = IoUs / len(valid_loader)
+    R = Rs / len(valid_loader)
+    loss = losses / len(valid_loader)
+    to_log("valid: \nIoU: {}\nRecall: {}".format(IoU, R))
+    writer.add_scalar("valid/loss", loss, 0)
+    writer.add_scalar("valid/IoU", IoU, 0)
+    writer.add_scalar("valid/R", R, 0)
 
     writer.close()
 
